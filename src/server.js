dc21621c80eafcb054d0b731fb2ee6d4fdc49edf
trannham <snaart1996@gmail.com>
2022-06-11 14:05:04 +0300

    structuring with MVC model

--- src/server.js
+++ src/server.js
@@ -1,25 +1,23 @@
 // const express = require("express");
 import express from "express";
 import configViewEngine from "./config/viewEngine";
+import initWebRoute from "./route/web";
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+//view engine
 configViewEngine(app);
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
-});
-app.get("/about", (req, res) => {
-  res.send("My name is Tran nham");
-});
+//init web route
+initWebRoute(app);
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
-
-//normal function
-app.listen(port, function () {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+//normal function
+// app.listen(port, function () {
+//   console.log(`Example app listening on port ${port}`);
+// });
