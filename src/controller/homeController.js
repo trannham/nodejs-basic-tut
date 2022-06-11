b44c8df1128bdd35c7ff3f5bb9e3bc0ee90a767c
trannham <snaart1996@gmail.com>
2022-06-11 15:30:17 +0300

    connect db

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,5 +1,17 @@
+import connection from "../config/connectDB";
+
 let getHomepage = (req, res) => {
-  return res.render("index.ejs");
+  //logic
+  let data = [];
+  connection.query("SELECT * FROM `users` ", function (err, results, fields) {
+    console.log(">> check mysql");
+    console.log(results); // results contains rows returned by server
+    data = results.map((row) => {
+      return row;
+    });
+    console.log(JSON.stringify(data));
+    return res.render("index.ejs", { dataUser: JSON.stringify(data) });
+  });
 };
 
 module.exports = {
