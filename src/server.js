2d8d50abdd4db25e0e1b440a3be423df2ed5aacd
trannham <snaart1996@gmail.com>
2022-06-12 15:14:27 +0300

    test morgan middleware packages

--- src/server.js
+++ src/server.js
@@ -3,13 +3,19 @@ import express from "express";
 import configViewEngine from "./config/viewEngine";
 import initWebRoute from "./route/web";
 import initAPIRoute from "./route/api";
+import morgan from "morgan";
+
 // import connection from "./config/connectDB";
 
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 8080;
-
+app.use((req, res, next) => {
+  console.log("method use: ", req.method);
+  next();
+});
+app.use(morgan("combined"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -21,6 +27,11 @@ initWebRoute(app);
 
 //init api route
 initAPIRoute(app);
+
+//handle middleware 404
+app.use((req, res) => {
+  return res.render("404.ejs");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
